76e1dc29dac6987c2d74e3b1c30b2d2db3236df5
Nater0214 <gamer.nater0214@gmail.com>
2021-12-21 23:36:52 -0500

    First working (that is a stretch) update

--- Website Downloader.py
+++ Website Downloader.py
@@ -1,1 +1,15 @@
-pass
+import requests #imports
+
+website = input("Website to download: ") #get the website data
+data = requests.get(website)
+data = data.text
+
+name = input("Name of the file: ") #get the name of the file
+
+file = open(name + ".html", "a")
+
+for i in data:
+    try:
+        file.write(i)
+    except:
+        pass
